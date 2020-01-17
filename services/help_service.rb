3cf025bba6fc006e398cd85e3c3d6085a7ec80e2
joao_felipe <joaofelipe1294@gmail.com>
2020-01-17 16:07:25 -0300

    Adicionada chamada ao help_service

--- services/help_service.rb
+++ services/help_service.rb
@@ -7,7 +7,7 @@ module TranslateBot
       seconf_line = "1-) traduzir X inglês para português\n"
       third_line = "2-) como se fala X em inglês\n"
       "#{first_line}#{seconf_line}#{third_line}"
-    end
+    end 
 
   end
 
